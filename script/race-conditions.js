8167c094ce9fa233926e0507083c6086bbd73986
Lucas Nagle <lnagle@gmail.com>
2016-12-08 10:56:58 -0600

    Begin manual testing

--- script/race-conditions.js
+++ script/race-conditions.js
@@ -2,59 +2,132 @@
 
 "use strict";
 
-var assert, config, Plugin, testScenarios;
+var config, DDQ, paramKey, testScenarios;
 
-assert = require("assert");
 config = require("./manual-testing-config");
-Plugin = require("..");
+DDQ = require("..");
+paramKey = {
+    close: [
+        (instance, test) => {
+            console.log(`Running next: ${test}`);
+            runCommands(instance, test);
+        }
+    ]
+};
 testScenarios = [
+    // [
+    //     "open",
+    //     "close"
+    // ]
+    // ,
+    // [
+    //     "open",
+    //     "open"
+    // ]
+    // ,
+    // [
+    //     "close"
+    // ]
+    // ,
+    // [
+    //     "sendMessage"
+    // ]
+    // ,
     [
         "open",
-        "close"
-    ], [
-        "open",
-        "open"
-    ], [
-        "close",
-        "close"
-    ], [
-        "sendMessage"
-    ], [
-        "open",
         "sendMessage",
         "listenStart",
         "sendMessage",
-        "close"
-    ], [
-        "listenStart"
-    ], [
-        "open",
-        "listenStart",
-        "listenStart"
-    ], [
-        "open",
-        "close",
         "close"
     ]
+    // ,
+    // [
+    //     "listenStart"
+    // ]
+    // ,
+    // [
+    //     "open",
+    //     "listenStart",
+    //     "listenStart"
+    // ]
+    // ,
+    // [
+    //     "open",
+    //     "close",
+    //     "close"
+    // ]
 ];
 
 /* eslint-disable require-jsdoc */
-function scenarioTest(test) {
-    var instance;
+// function scenarioTest(test) {
+//     var instance;
+//
+//     instance = new DDQ(config);
+//     instance.on("error", (err) => {
+//         console.log(err.message);
+//     });
+//     instance.on("data", (data) => {
+//         console.log(data);
+//     });
+//     test.forEach((command) => {
+//         instance[command](() => {
+//             console.log(command);
+//         });
+//     });
+//
+// }
+//
+function runCommands(instance, test) {
+    var mostRecentCommand;
 
-    instance = new Plugin(config);
+    if (test.length) {
+        // Remove the first command from test and set it to mostRecentCommand
+        mostRecentCommand = test.shift();
+    } else {
+        console.log("Finished running commands.");
 
-    test.forEach((command) => {
-        instance[command](() => {
-            console.log(command);
-        });
-    });
+        return;
+    }
+
+    instance[mostRecentCommand].apply(null, paramKey[mostRecentCommand]);
 }
 
 function runner(tests) {
     tests.forEach((test) => {
-        scenarioTest(test);
+        var instance;
+
+        instance = new DDQ(config);
+        instance.on("error", () => {
+            if (instance.backend.connection) {
+                instance.backend.connection.destroy();
+            } else {
+                console.log("ERROR LISTENER ACTIVATED");
+            }
+        });
+        instance.on("data", (data) => {
+            console.log(data);
+        });
+
+        runCommands(instance, test);
     });
 }
 
 runner(testScenarios);
+
+
+// var instance;
+//
+// instance = new DDQ(config);
+//
+// instance.on("error", (err) => {
+//     console.log(err.message);
+// });
+//
+// instance.on("data", (data) => {
+//     console.log(data);
+// });
+//
+// instance.open(() => {
+//     instance.close();
+// });
+// instance.close();
